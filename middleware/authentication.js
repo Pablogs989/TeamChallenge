e16d8586cf3c3800e325cc30f870d94aedcf3b90
Pablogs989 <pablogonsan2001@gmail.com>
2024-05-16 09:31:14 +0200

    carpetas

---
+++ middleware/authentication.js
@@ -0,0 +1,57 @@
+const User = require('../models/User');
+const Comment = require('../models/Comment');
+const Post = require('../models/Post');
+const jwt = require('jsonwebtoken');
+require("dotenv").config();
+const { JWT_SECRET } = process.env
+
+const authentication = async (req, res, next) => {
+    try {
+        const token = req.headers.authorization;
+        const payload = jwt.verify(token, JWT_SECRET);
+        const user = await User.findOne({ _id: payload._id, tokens: token });
+        if (!user) {
+            return res.status(401).send({ message: 'You are not authorized' });
+        }
+        req.user = user;
+        next();
+    } catch (error) {
+        console.error(error)
+        return res.status(500).send({ error, message: 'There was a problem with the token' })
+    }
+}
+const isAdmin = async (req, res, next) => {
+    const admins = ['admin', 'superadmin'];
+    if (!admins.includes(req.user.role)) {
+        return res.status(403).send({
+            message: "You don't have permits"
+        });
+    }
+    next();
+}
+const isAuthorPost = async (req, res, next) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        if (post.userId.toString() !== req.user._id.toString()) {
+            return res.status(403).send({ message: "You don't have permits" });
+        }
+        next();
+    } catch (error) {
+        console.error(error)
+        return res.status(500).send({ error, message: 'There has been an issue checking the authorship' })
+    }
+}
+const isAuthorComment = async (req, res, next) => {
+    try {
+        const comment = await Comment.findById(req.params._id);
+        if (comment.userId.toString() !== req.user._id.toString()) {
+            return res.status(403).send({ message: "You don't have permits" });
+        }
+        next();
+    } catch (error) {
+        console.error(error)
+        return res.status(500).send({ error, message: "There has been an issue checking the authorship" })
+    }
+}
+
+module.exports = { authentication, isAdmin, isAuthorComment, isAuthorPost };
