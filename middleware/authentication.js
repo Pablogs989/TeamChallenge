70210711fbd915de78eb45e18a96f23a2e3aca5c
Pablogs989 <pablogonsan2001@gmail.com>
2024-05-16 09:48:43 +0200

    Update authentication.js

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -1,6 +1,3 @@
-const User = require('../models/User');
-const Comment = require('../models/Comment');
-const Post = require('../models/Post');
 const jwt = require('jsonwebtoken');
 require("dotenv").config();
 const { JWT_SECRET } = process.env
@@ -20,38 +17,5 @@ const authentication = async (req, res, next) => {
         return res.status(500).send({ error, message: 'There was a problem with the token' })
     }
 }
-const isAdmin = async (req, res, next) => {
-    const admins = ['admin', 'superadmin'];
-    if (!admins.includes(req.user.role)) {
-        return res.status(403).send({
-            message: "You don't have permits"
-        });
-    }
-    next();
-}
-const isAuthorPost = async (req, res, next) => {
-    try {
-        const post = await Post.findById(req.params.id);
-        if (post.userId.toString() !== req.user._id.toString()) {
-            return res.status(403).send({ message: "You don't have permits" });
-        }
-        next();
-    } catch (error) {
-        console.error(error)
-        return res.status(500).send({ error, message: 'There has been an issue checking the authorship' })
-    }
-}
-const isAuthorComment = async (req, res, next) => {
-    try {
-        const comment = await Comment.findById(req.params._id);
-        if (comment.userId.toString() !== req.user._id.toString()) {
-            return res.status(403).send({ message: "You don't have permits" });
-        }
-        next();
-    } catch (error) {
-        console.error(error)
-        return res.status(500).send({ error, message: "There has been an issue checking the authorship" })
-    }
-}
 
-module.exports = { authentication, isAdmin, isAuthorComment, isAuthorPost };
+module.exports = { authentication };
